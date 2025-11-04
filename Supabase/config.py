7984effe109f79d0dce698bbf1bea032f971d391
BastienDeMeyer <bastien.demeyer@ugent.be>
2025-11-04 16:51:23 +0100

    Supabase key

---
+++ Supabase/config.py
@@ -0,0 +1,5 @@
+class Config: 
+    SECRET_KEY = "Supabase_secret_Key"
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:InDeAuto2025!@db.bpbvlfptoacijyqyugew.supabase.co:5432/postgres"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    
